e8249fc47ed206ac1f854ed7b82036c4408d6da0
Sathishkumar <be.sathish@gmail.com>
2019-12-21 22:10:44 +0530

    Added header in the Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Fib Calculator</h1>
         <Link to="/">Home</Link> | 
         <Link to="/otherpage">Other Page</Link>
         <Route exact path="/" component={Fib} />
